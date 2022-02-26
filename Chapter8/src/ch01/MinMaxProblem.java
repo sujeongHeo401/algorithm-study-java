d7250a5e4bc1e278746e47319aad13f59f9fe813
sujeongHeo401 <gotnwjd40@gmail.com>
2022-02-27 03:54:19 +0900

    뭉뎨1번 - 뽀문 하나로 최대/최소값 찾기 헤헤

---
+++ Chapter8/src/ch01/MinMaxProblem.java
@@ -0,0 +1,34 @@
+package ch01;
+
+/// 반복문 한번만 돌려서 min, max 값 차자라...
+public class MinMaxProblem {
+
+	public static void main(String[] args) {
+		
+		int[] numbers = {10, 55, 23, 2, 79, 101, 16, 82, 30, 45};
+		
+		int min = numbers[0];
+		int max = numbers[0];
+		
+		int minPos = 0;
+		int maxPos = 0;
+		
+		for(int i = 1; i < numbers.length; i++) {
+			if(min > numbers[i]) {
+				min = numbers[i];
+				minPos = i + 1;
+			}
+			if(max < numbers[i]) {
+				max = numbers[i];
+				maxPos = i;
+			}
+		}
+		
+		System.out.println("가장 큰 값은 : " + max + "이고, 위치는 " + maxPos + "번째 입니다.");
+		System.out.println("가장 큰 값은 : " + min + "이고, 위치는 " + minPos + "번째 입니다.");
+		
+		// TODO Auto-generated method stub
+
+	}
+
+}
